a10a3e24d81ea1b2e1727972d114a852a1c16223
afiya_iram <afiya_iram@vecros.com>
2025-04-22 01:26:52 +0530

    learnt about dynamic routes and useParams

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import {Link, Routes, Route} from "react-router-dom"
 // import './App.css'
 import Home from "./components/Home"
 import About from "./components/About"
+import BookList from "./components/BookList"
+import Book from "./components/Book"
 
 function App() {
 
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
+        <Route path="/books" element={<BookList/>}/>
+        <Route path="/books/:id" element={<Book/>}/>
         <Route/>
         <Route/>
       </Routes>
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Book = () => {
+  const {id}= useParams();
   return (
-    <div>Book</div>
+    <h1>Book {id}</h1>
   )
 }
 
