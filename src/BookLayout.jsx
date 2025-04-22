01657cdfda56468254829ab344817f505ee196fb
afiya_iram <afiya_iram@vecros.com>
2025-04-23 01:17:47 +0530

    useSearchParams

--- src/BookLayout.jsx
+++ src/BookLayout.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useSearchParams } from 'react-router-dom'
 
 const BookLayout = () => {
-  const [number, setNumber] = useState(3);
+  // const [number, setNumber] = useState(3);
+  const [searchParams, setSearchParams]=useSearchParams({n:3});
+  const number = searchParams.get("n");
   return (
     <>
     <h1>BookLayout</h1>
@@ -10,14 +12,21 @@ const BookLayout = () => {
     <br/>
     <Link to="/books/2">Book 2</Link>
     <br/>
+    {/* <Link to={`/books/${number}`}>Book {number}</Link> */}
     <Link to={`/books/${number}`}>Book {number}</Link>
     <Link to="/books/new"> New Book</Link>
     <Outlet context={{obj:{ hello : "world"}}}/>
 
-    <input
+    {/* <input
     type="number"
     value={number}
     onChange={e=>setNumber(e.target.value)}
+    
+    /> */}
+    <input
+    type="number"
+    value={number}
+    onChange={e=>setSearchParams({n:e.target.value})}
     
     />
     
