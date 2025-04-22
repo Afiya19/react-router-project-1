c657e8c6f0e21997dbca1228f101b43b2a8b1bc2
afiya_iram <afiya_iram@vecros.com>
2025-04-23 01:10:44 +0530

    usestate

--- src/BookLayout.jsx
+++ src/BookLayout.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 const BookLayout = () => {
+  const [number, setNumber] = useState(3);
   return (
     <>
     <h1>BookLayout</h1>
@@ -9,8 +10,16 @@ const BookLayout = () => {
     <br/>
     <Link to="/books/2">Book 2</Link>
     <br/>
+    <Link to={`/books/${number}`}>Book {number}</Link>
     <Link to="/books/new"> New Book</Link>
     <Outlet context={{obj:{ hello : "world"}}}/>
+
+    <input
+    type="number"
+    value={number}
+    onChange={e=>setNumber(e.target.value)}
+    
+    />
     
     </>
   )
